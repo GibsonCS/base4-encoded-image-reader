b495488b28d28fc00f7ec28b3428cb77df906ed8
GibsonCS <gibson.cruzds@gmail.com>
2025-04-25 15:33:11 -0300

    chore: configs sqlite and seed function to create tables into ./src/shared/database/db.sqlite

---
+++ src/shared/database/db.ts
@@ -0,0 +1,21 @@
+import { DatabaseSync } from "node:sqlite";
+
+export const database = new DatabaseSync("./src/shared/database/db.sqlite");
+export const runSeed = () => {
+  database.exec(`
+            DROP TABLE IF EXISTS customer;
+            DROP TABLE IF EXISTS measurement;`);
+  database.exec(`
+            CREATE TABLE customer(
+                customer_code string PRIMARY KEY)
+            `);
+  database.exec(`
+            CREATE TABLE measurement(
+                measure_uuid string PRIMARY KEY,
+                measure_datetime date,
+                measure_type string,
+                has_confirmed boolean,
+                customer_code string NOT NULL,
+                FOREIGN KEY (customer_code) REFERENCES measurement (customer_code)
+  )`);
+};
