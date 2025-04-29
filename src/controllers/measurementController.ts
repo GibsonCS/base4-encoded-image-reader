e575b566a33d1bf3a1a4f6c2394cf9e5f820ec42
GibsonCS <gibson.cruzds@gmail.com>
2025-04-29 14:57:41 -0300

    refactor: meaurement controller

--- src/controllers/measurementController.ts
+++ src/controllers/measurementController.ts
@@ -1,9 +1,10 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
-import type { MeasurementService } from "../services/measurementService.ts";
+import type { MeasurementService } from "../services/measurementService.js";
 
 export class MeasurementController {
   constructor(private service: MeasurementService) {}
   handleMeasurement = async (request: FastifyRequest, reply: FastifyReply) => {
+    console.log(request.body);
     this.service.getMeasurement(request.body);
     reply.code(200).send({
       image_url: "url_image",
