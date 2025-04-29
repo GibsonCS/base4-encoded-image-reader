11f771c380bc8142605dee0a49e40ccef45cc4ce
GibsonCS <gibson.cruzds@gmail.com>
2025-04-29 14:36:20 -0300

    feat: creates handle measurement controller

---
+++ src/modules/measurement/controllers/measurementController.ts
@@ -0,0 +1,14 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import type { MeasurementService } from "../services/measurementService.ts";
+
+export class MeasurementController {
+  constructor(private service: MeasurementService) {}
+  handleMeasurement = async (request: FastifyRequest, reply: FastifyReply) => {
+    this.service.getMeasurement(request.body);
+    reply.code(200).send({
+      image_url: "url_image",
+      measure_value: 500,
+      measure_uuid: "uuid123",
+    });
+  };
+}
