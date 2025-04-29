0778865cfa29495ccb99968aebcdc71fb065cb6d
GibsonCS <gibson.cruzds@gmail.com>
2025-04-29 15:00:25 -0300

    chore: change port server and import register routes function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import fastify from "fastify";
+import { routesRegistration } from "./routes/index.js";
 import { runSeed } from "./shared/database/db.ts";
 
 export const server = fastify();
+routesRegistration(server);
+
 const startServer = async () => {
-  await server.listen({ port: 80 });
-  console.log("Server listening at 80");
+  await server.listen({ port: 3000 });
+  console.log("Server listening at 3000");
 };
 startServer();
 runSeed();
