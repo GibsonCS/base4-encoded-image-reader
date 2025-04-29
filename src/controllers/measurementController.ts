5b007baae2ed12c9c37ec614e73afb5090869631
GibsonCS <gibson.cruzds@gmail.com>
2025-04-29 17:41:13 -0300

    refactor: src/controllers/measurementController.ts

--- src/controllers/measurementController.ts
+++ src/controllers/measurementController.ts
@@ -1,15 +1,31 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import type { MeasurementService } from "../services/measurementService.js";
+import { measurementSchema } from "../schemas/measurementSchema.ts";
 
 export class MeasurementController {
   constructor(private service: MeasurementService) {}
   handleMeasurement = async (request: FastifyRequest, reply: FastifyReply) => {
-    console.log(request.body);
-    this.service.getMeasurement(request.body);
-    reply.code(200).send({
-      image_url: "url_image",
-      measure_value: 500,
-      measure_uuid: "uuid123",
-    });
+    const result = measurementSchema.safeParse(request.body);
+
+    if (!result.success) {
+      reply.code(400).send({
+        error_code: "INVALID_DATA",
+        error_description:
+          "Os dados fornecidos no corpo da requisição são inválidos",
+      });
+      return;
+    }
+
+    this.service.getMeasurement(result.data);
+
+    try {
+      reply.code(200).send({
+        image_url: "url_image",
+        measure_value: 500,
+        measure_uuid: "uuid123",
+      });
+    } catch (error) {
+      reply.code(500);
+    }
   };
 }
