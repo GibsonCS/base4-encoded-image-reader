947506bc1658bc4867ebebf2e1d08ae43b9d6779
GibsonCS <gibson.cruzds@gmail.com>
2025-04-29 14:17:43 -0300

    chore: removes controller from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import fastify from "fastify";
 import { runSeed } from "./shared/database/db.ts";
-import { MeasurementController } from "./modules/measurement/controllers/measurementController.ts";
 
 export const server = fastify();
-
-const controller = new MeasurementController();
-
-server.post("/upload", controller.handleMeasurement);
-
 const startServer = async () => {
   await server.listen({ port: 80 });
   console.log("Server listening at 80");
