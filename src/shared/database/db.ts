402e1563b327d50bfde3156a300579a588aef720
GibsonCS <gibson.cruzds@gmail.com>
2025-05-01 22:01:41 -0300

    fix: database seed in the src/shared/db.ts

--- src/shared/database/db.ts
+++ src/shared/database/db.ts
@@ -3,8 +3,10 @@ import { DatabaseSync } from "node:sqlite";
 export const database = new DatabaseSync("./src/shared/database/db.sqlite");
 export const runSeed = () => {
   database.exec(`
-            DROP TABLE IF EXISTS customer;
-            DROP TABLE IF EXISTS measurement;`);
+    DROP TABLE IF EXISTS measurement;
+    DROP TABLE IF EXISTS customer;
+    `);
+
   database.exec(`
             CREATE TABLE customer(
                 customer_code string PRIMARY KEY)
@@ -13,9 +15,10 @@ export const runSeed = () => {
             CREATE TABLE measurement(
                 measure_uuid string PRIMARY KEY,
                 measure_datetime date,
+                measure_value,
                 measure_type string,
                 has_confirmed boolean,
                 customer_code string NOT NULL,
-                FOREIGN KEY (customer_code) REFERENCES measurement (customer_code)
+                FOREIGN KEY (customer_code) REFERENCES customer (customer_code)
   )`);
 };
