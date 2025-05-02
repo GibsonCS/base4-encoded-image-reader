b38def5e2ec79d567e7b6e2db4a0a949bdc94b79
GibsonCS <gibson.cruzds@gmail.com>
2025-05-01 22:03:44 -0300

    feat: add get images route and refator src/routes/measurementRoter.ts

--- src/routes/measurementRouter.ts
+++ src/routes/measurementRouter.ts
@@ -1,10 +1,16 @@
 import type { FastifyInstance } from "fastify";
 import { MeasurementController } from "../controllers/measurementController.ts";
 import { MeasurementService } from "../services/measurementService.ts";
+import { CustomerRepository } from "../repositories/customerRepository.ts";
+import { database } from "../shared/database/db.ts";
+import { MeasurementRepository } from "../repositories/measurementRepository.ts";
 
-const measurementController = new MeasurementController(
-  new MeasurementService()
-);
+const customerRepository = new CustomerRepository(database)
+const measurementRepository = new MeasurementRepository(database)
+const measurementService = new MeasurementService(customerRepository, measurementRepository)
+const measurementController = new MeasurementController(measurementService)
+
 export const measurementRoutes = async (server: FastifyInstance) => {
   server.post("/upload", measurementController.handleMeasurement);
+  server.get(`/images/:id${'.jpeg'}`, measurementController.handleTempImage)
 };
