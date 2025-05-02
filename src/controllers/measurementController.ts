75dbaa2a0ca76ec6caf57e5500ae6fc56ae5f8b8
GibsonCS <gibson.cruzds@gmail.com>
2025-05-01 22:23:41 -0300

    feat: implement measurementController

--- src/controllers/measurementController.ts
+++ src/controllers/measurementController.ts
@@ -1,38 +1,42 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import type { MeasurementService } from "../services/measurementService.js";
 import { measurementSchema } from "../schemas/measurementSchema.ts";
+import { readFile } from 'node:fs/promises'
 
 export class MeasurementController {
-  constructor(private service: MeasurementService) {}
+  constructor(private service: MeasurementService) { }
   handleMeasurement = async (request: FastifyRequest, reply: FastifyReply) => {
+    //we use safeParse to validade data safely, receiving a object with success, data or error.
     const result = measurementSchema.safeParse(request.body);
 
-    if (!result.success) {
-      reply.code(400).send({
-        error_code: "INVALID_DATA",
-        error_description:
-          "Os dados fornecidos no corpo da requisição são inválidos",
-      });
-      return;
+    const INVALID_DATA = {
+      error_code: "INVALID_DATA",
+      error_description: "Os dados fornecidos no corpo da requisição são inválidos",
     }
+
+    if (result.error.issues[0].path[0] === "measure_type") return reply.code(400).send({ message: "Você precisa escolher somente entre WATER ou GAS" })
+    if (!result.success) return reply.code(400).send(INVALID_DATA);
 
     const response: any = await this.service.getMeasurement(result.data);
 
-    if (response.error_code === "DOUBLE_REPORT") {
-      reply.code(409).send({
-        error_code: "DOUBLE_REPORT",
-        error_description: "Leitura do mês já realizada",
-      });
-      return;
+    const ERRO_DOUBLE_REPORT = {
+      error_code: "DOUBLE_REPORT",
+      error_description: "Leitura do mês já realizada",
     }
-    try {
-      reply.code(200).send({
-        image_url: "url_image",
-        measure_value: 500,
-        measure_uuid: "uuid123",
-      });
-    } catch (error) {
-      reply.code(500);
-    }
+
+    if (response.error_code === "DOUBLE_REPORT") return reply.code(409).send(ERRO_DOUBLE_REPORT);
+
+    reply.code(200).send(response);
   };
+
+  handleTempImage = async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id } = request.params as { id: string }
+
+    const imagesLog = JSON.parse(await readFile(`${process.cwd()}/src/images/imageLog.json`, 'utf8'))
+    const [log] = imagesLog.filter((i: any) => i.imageId === id)
+
+    if (Date.now() > log.expiresAt) return reply.code(500).send({ message: 'Link Expirado' })
+
+    reply.header('Content-type', 'image/jpeg').code(200).send(await readFile(`${process.cwd()}/src/images/${id}.jpeg`))
+  }
 }
