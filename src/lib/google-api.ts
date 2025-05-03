037b06830508f342af7615d2ad5ef5060ac589ad
GibsonCS <gibson.cruzds@gmail.com>
2025-05-02 23:12:58 -0300

    feat: creates makeRequestToGoogleAI in the src/lib

---
+++ src/lib/google-api.ts
@@ -0,0 +1,20 @@
+import { GoogleGenAI } from '@google/genai'
+
+export const makeRequestToGoogleAI = async (imageBase64: string) => {
+    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
+    const contents = [
+        {
+            inlineData: {
+                mimeType: "image/jpeg",
+                data: imageBase64,
+            },
+        },
+        { text: "Return only number this image" },
+    ];
+
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: contents
+    })
+    return response.text
+}
