c7c4252e38f76c32955681f7b2f7b1de9ae6150e
GibsonCS <gibson.cruzds@gmail.com>
2025-05-02 23:22:03 -0300

    feat: finish implementation of the getMeasurement

--- src/services/measurementService.ts
+++ src/services/measurementService.ts
@@ -1,54 +1,36 @@
-import { GoogleGenAI } from '@google/genai'
 import type { CustomerRepository } from "../repositories/customerRepository.ts";
-import type { Measurement } from "../schemas/measurementSchema.ts";
-import type { MeasurementRepository } from '../repositories/measurementRepository.ts';
+import type { Measurement } from "../schemas/measurementSchema.js";
+import type { MeasurementRepository } from '../repositories/measurementRepository.js';
 import crypto from 'node:crypto'
 import { writeFile } from 'node:fs/promises'
 import { readFile } from 'node:fs/promises';
 import dayjs from 'dayjs'
+import { makeRequestToGoogleAI } from '../lib/google-api.js';
 
 export class MeasurementService {
   constructor(private customerRepository: CustomerRepository, private measurementRepository: MeasurementRepository) { }
 
   getMeasurement = async (measureData: Measurement) => {
-    const customer = this.customerRepository.findById(measureData.customer_code)
+    const { image, measure_datetime, measure_type, customer_code } = measureData
+
+    const imageId: string = crypto.randomUUID()
+    await writeFile(`${process.cwd()}/src/images/${imageId}.jpeg`, Buffer.from(image, 'base64'))
+
+    const customer = this.customerRepository.findById(customer_code)
     if (!customer) {
+      this.customerRepository.save(measureData)
 
-      const registedCustomer = this.customerRepository.save(measureData)
-      const { image, measure_datetime, measure_type } = measureData
-
-      // const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
-      // const contents = [
-      //   {
-      //     inlineData: {
-      //       mimeType: "image/jpeg",
-      //       data: image,
-      //     },
-      //   },
-      //   { text: "Return only number this image" },
-      // ];
-
-      // const response = await ai.models.generateContent({
-      //   model: "gemini-2.0-flash",
-      //   contents: contents
-      // })
-
+      const valueOfMeasure = await makeRequestToGoogleAI(image)
       const measurement = {
         measure_uuid: crypto.randomUUID(),
         measure_datetime,
-        measure_value: 357,  //parseInt(response.text),
+        measure_value: parseInt(valueOfMeasure),
         measure_type,
         has_confirmed: 0,
-        customer_code: registedCustomer.customer_code
+        customer_code
       }
 
-      const savedMeasurementt = this.measurementRepository.save(measurement)
-
-      const { measure_uuid, measure_value } = savedMeasurementt
-
-      const imageId: string = crypto.randomUUID()
-
-      await writeFile(`${process.cwd()}/src/images/${imageId}.jpeg`, Buffer.from(measureData.image, 'base64'))
+      this.measurementRepository.save(measurement)
 
       const log = {
         imageId,
@@ -63,28 +45,54 @@ export class MeasurementService {
 
       return {
         image_url: `http://localhost:3000/images/${imageId}.jpeg`,
-        measure_value,
-        measure_uuid
+        measure_value: measurement.measure_value,
+        measure_uuid: measurement.measure_uuid
       }
     }
 
+    const measurements = this.measurementRepository.findByCustomerCode(customer_code)
+    const lastMeasurement = measurements[measurements.length - 1]
 
+    const isWithin30Days = (lastMeasurement: string, currentMeasurement: string) => {
+      const lastMeasurementTimeStemp = new Date(lastMeasurement).valueOf()
+      const todayLess30Days = dayjs(currentMeasurement).subtract(30, 'day').valueOf()
+      return todayLess30Days < lastMeasurementTimeStemp
+    }
 
-    // const measurements = this.measurementRepository.findByCustomerCode("customerCodeUUID2")
+    const DOUBLE_REPORT = {
+      error_code: "DOUBLE_REPORT",
+      error_description: "Leitura do mês já realizada"
+    }
 
-    // const isWithin30Days = (date: string) => {
-    //   const dateTimestemp = new Date(date)
-    //   const now = new Date()
+    if ((isWithin30Days(lastMeasurement.measure_datetime, measure_datetime))) return DOUBLE_REPORT
 
-    //   console.log(dayjs(dateTimestemp).isBefore(now, 'days'))
+    const valueOfMeasure = await makeRequestToGoogleAI(image)
+    const measurement = {
+      measure_uuid: crypto.randomUUID(),
+      measure_datetime,
+      measure_value: parseInt(valueOfMeasure),
+      measure_type,
+      has_confirmed: 0,
+      customer_code: measureData.customer_code
+    }
 
-    //   return
-    // }
+    this.measurementRepository.save(measurement)
 
-    // console.log(isWithin30Days(measurements.measure_datetime))
+    const log = {
+      imageId,
+      expiresAt: Date.now() + 1 * 60 * 1000
+    }
 
+    const imagesLog = JSON.parse(await readFile(`${process.cwd()}/src/images/imageLog.json`, 'utf8'))
 
+    imagesLog.push(log)
 
-    return {}
+    await writeFile(`${process.cwd()}/src/images/imageLog.json`, JSON.stringify(imagesLog))
+
+    return {
+      image_url: `http://localhost:3000/images/${imageId}.jpeg`,
+      measure_value: measurement.measure_value,
+      measure_uuid: measurement.measure_uuid
+    }
   };
 }
