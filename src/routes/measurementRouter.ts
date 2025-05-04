f02855eed30adbd3582e449e654c21b97eeb336f
GibsonCS <gibson.cruzds@gmail.com>
2025-05-03 22:56:14 -0300

    feat: creates /confirm router

--- src/routes/measurementRouter.ts
+++ src/routes/measurementRouter.ts
@@ -5,12 +5,16 @@ import { CustomerRepository } from "../repositories/customerRepository.ts";
 import { database } from "../shared/database/db.ts";
 import { MeasurementRepository } from "../repositories/measurementRepository.ts";
 
-const customerRepository = new CustomerRepository(database)
-const measurementRepository = new MeasurementRepository(database)
-const measurementService = new MeasurementService(customerRepository, measurementRepository)
-const measurementController = new MeasurementController(measurementService)
+const customerRepository = new CustomerRepository(database);
+const measurementRepository = new MeasurementRepository(database);
+const measurementService = new MeasurementService(
+  customerRepository,
+  measurementRepository
+);
+const measurementController = new MeasurementController(measurementService);
 
 export const measurementRoutes = async (server: FastifyInstance) => {
   server.post("/upload", measurementController.handleMeasurement);
-  server.get(`/images/:id${'.jpeg'}`, measurementController.handleTempImage)
+  server.get(`/images/:id${".jpeg"}`, measurementController.handleTempImage);
+  server.patch("/confirm", measurementController.handleConfirmMeasurement);
 };
