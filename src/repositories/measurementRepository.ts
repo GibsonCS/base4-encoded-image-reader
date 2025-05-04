d21d596b38670956098f6c808e1422bfbb0f31ca
GibsonCS <gibson.cruzds@gmail.com>
2025-05-04 16:47:59 -0300

    feat: add update function in measurement repository

--- src/repositories/measurementRepository.ts
+++ src/repositories/measurementRepository.ts
@@ -1,52 +1,64 @@
 import type { DatabaseSync } from "node:sqlite";
-import type { Measurement } from "../schemas/measurementSchema.ts";
+import type {
+  ConfirmMeasurement,
+  Measurement,
+} from "../schemas/measurementSchema.ts";
 import type { BaseRepostiory } from "./baseRepository.ts";
 
 interface MeasurementEntity {
-    measure_uuid: string;
-    measure_datetime: string;
-    measure_value: number
-    measure_type: string;
-    has_confirmed: number;
-    customer_code: string
+  measure_uuid?: string;
+  measure_datetime?: string;
+  measure_value?: number;
+  measure_type?: string;
+  has_confirmed?: number;
+  customer_code?: string;
 }
 
-export class MeasurementRepository implements BaseRepostiory<Measurement> {
-    constructor(private database: DatabaseSync) { }
+export class MeasurementRepository
+  implements BaseRepostiory<Measurement | ConfirmMeasurement>
+{
+  constructor(private database: DatabaseSync) {}
 
-    save(measurement: MeasurementEntity): MeasurementEntity {
-        const statement = this.database.prepare(`
+  save(measurement: MeasurementEntity): MeasurementEntity {
+    const statement = this.database.prepare(`
                 INSERT INTO measurement (measure_uuid, measure_datetime, measure_value, measure_type, has_confirmed, customer_code)
                 VALUES (@measure_uuid,@measure_datetime,@measure_value,@measure_type,@has_confirmed,@customer_code)
-            `)
-        statement.run({ ...measurement });
+            `);
+    statement.run({ ...measurement });
 
-        return measurement
-    }
+    return measurement;
+  }
 
-    findById(id: string): Measurement {
-        // const statement = this.database.prepare(`
-        //         SELECT * from measurement where customer_code = ?
-        //     `)
+  async findById(id: string): Promise<MeasurementEntity> {
+    const statement = this.database.prepare(`
+                SELECT * from measurement where measure_uuid = ?
+            `);
+    const measurement = statement.get(id);
+    return measurement;
+  }
 
-        return
-
-    }
-
-    findByCustomerCode(customerId: string): Measurement[] {
-        const statement = this.database.prepare(`
+  findByCustomerCode(customerId: string): Measurement[] {
+    const statement = this.database.prepare(`
                 SELECT * FROM measurement WHERE customer_code = ?
-            `)
+            `);
 
-        const measurements = statement.all(customerId)
-        return measurements
-    }
+    const measurements = statement.all(customerId);
+    return measurements;
+  }
 
-    findAll(): { image?: string; customer_code?: string; measure_datetime?: string; measure_type?: string; }[] {
-        throw new Error("Method not implemented.");
-    }
-    update(obj: { image?: string; customer_code?: string; measure_datetime?: string; measure_type?: string; }): { image?: string; customer_code?: string; measure_datetime?: string; measure_type?: string; } {
-        throw new Error("Method not implemented.");
-    }
-
+  findAll(): {
+    image?: string;
+    customer_code?: string;
+    measure_datetime?: string;
+    measure_type?: string;
+  }[] {
+    throw new Error("Method not implemented.");
+  }
+  update(obj: ConfirmMeasurement): MeasurementEntity {
+    const statement = this.database.prepare(`
+            UPDATE measurement set measure_value = ?, has_confirmed = ? where measure_uuid = ?
+        `);
+    statement.run(obj.confirmed_value, 1, obj.measure_uuid);
+    return;
+  }
 }
